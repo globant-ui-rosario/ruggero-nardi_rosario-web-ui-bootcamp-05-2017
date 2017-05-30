1e4bca1481e5b286cb4a8071f885162a4c60e92c
Ruggero Nardi <s.ruggeronardi@gmail.com>
2017-05-30 20:29:05 -0300

    Topic 1 - Exercise 2. Added button to index.html with alert onclick

--- 01-JS-DOM-APIs/fade-in.js
+++ 01-JS-DOM-APIs/fade-in.js
@@ -1,4 +1,11 @@
-window.onload = function () {
+window.onload = function() {
   let fade_in_text = document.getElementById("fade-in-text");
   fade_in_text.setAttribute("class", "visible-text");
+}
+
+const button_to_index = document.getElementById("button-to-index");
+
+button_to_index.onclick = function() {
+  alert("Button Clicked");
+  window.location.href="index.html";
 }
