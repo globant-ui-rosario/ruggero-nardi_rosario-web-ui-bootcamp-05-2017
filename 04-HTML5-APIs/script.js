d23590581952b8fa71c66e57521bfe51ff697aac
Ruggero Nardi <s.ruggeronardi@gmail.com>
2017-06-05 17:07:27 -0300

    Topic 4 - Exercise 2. Added the posibility to drop text files in the text area

--- 04-HTML5-APIs/script.js
+++ 04-HTML5-APIs/script.js
@@ -2,8 +2,29 @@ let textdb;
 function start() {
   const buttonSave = document.getElementById('button-save');
   const buttonClear = document.getElementById('button-clear');
+  const textAreaDrop = document.getElementById('text');
+  textAreaDrop.addEventListener("dragover", makeDragable);
+  textAreaDrop.addEventListener("drop", updateValue);
   buttonSave.addEventListener('click', addObject);
   buttonClear.addEventListener('click', clearDB);
+  function makeDragable(event) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'copy';
+  }
+  function updateValue(event) {
+    event.preventDefault();
+    let fileDroped = event.dataTransfer.files;
+    if (fileDroped[0].type === 'text/plain') {
+      let reader = new FileReader();
+      reader.readAsText(fileDroped[0]);
+      reader.onload = pasteText;
+    }
+  }
+  function pasteText(event) {
+    let textDroped = event.target.result;
+    let textArea = document.getElementById('text');
+    textArea.value = textDroped;
+  }
   const database = indexedDB.open('textDB');
   database.onsuccess = function (e) {
     textdb = e.target.result;
