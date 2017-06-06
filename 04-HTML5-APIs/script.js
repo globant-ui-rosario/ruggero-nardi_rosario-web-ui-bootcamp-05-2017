d6061057513b95905dc24e6cdefdd9f873462169
Ruggero Nardi <s.ruggeronardi@gmail.com>
2017-06-06 14:05:08 -0300

    Added WebSocket script

--- 04-HTML5-APIs/script.js
+++ 04-HTML5-APIs/script.js
@@ -1,12 +1,15 @@
 let textdb;
+const textArea = document.getElementById('text');
+const sendButton = document.getElementById('button-send');
+const connectionButton = document.getElementById('connection-button');
 function start() {
   const buttonSave = document.getElementById('button-save');
   const buttonClear = document.getElementById('button-clear');
-  const textAreaDrop = document.getElementById('text');
-  textAreaDrop.addEventListener("dragover", makeDragable);
-  textAreaDrop.addEventListener("drop", updateValue);
+  textArea.addEventListener("dragover", makeDragable);
+  textArea.addEventListener("drop", updateValue);
   buttonSave.addEventListener('click', addObject);
   buttonClear.addEventListener('click', clearDB);
+  init();
   function makeDragable(event) {
     event.preventDefault();
     event.dataTransfer.dropEffect = 'copy';
@@ -22,7 +25,6 @@ function start() {
   }
   function pasteText(event) {
     let textDroped = event.target.result;
-    let textArea = document.getElementById('text');
     textArea.value = textDroped;
   }
   const database = indexedDB.open('textDB');
@@ -51,6 +53,48 @@ function clearDB() {
   let storage = transactionRW.objectStore('text-area');
   storage.clear();
   localStorage.clear();
+}
+function init() {
+  websocket = new WebSocket('ws://echo.websocket.org');
+  websocket.onopen = function () { onOpen() };
+  websocket.onmessage = function (event) { onMessage(event) };
+  websocket.onerror = function (event) { onError(event) };
+}
+function onOpen() {
+  sendButton.addEventListener('click', sendMsg);
+  connectionButton.addEventListener('click', disconnect);
+  display('<span style="color: green;">CONNECTED</span>');
+}
+function onMessage(event) {
+  display('<span style="color: blue;">RESPONSE: ' + event.data + '</span>');
+}
+function onError(event) {
+  display('<span style="color: red;">ERROR:</span> ' + event.data);
+}
+function sendMsg() {
+  let text = textArea.value;
+  display("SENT: " + text);
+  websocket.send(text);
+  textArea.value = "";
+}
+function display(message) {
+  const output = document.getElementById('display-box');
+  let outputContainer = document.createElement("p");
+  outputContainer.setAttribute('class', 'output');
+  outputContainer.innerHTML = message;
+  output.appendChild(outputContainer);
+}
+function disconnect() {
+  websocket.close();
+  connectionButton.addEventListener('click', connect);
+  connectionButton.removeEventListener('click', disconnect);
+  display('<span style="color: red;">DISCONNECTED</span>');
+  connectionButton.innerHTML = 'CONNECT'
+}
+function connect() {
+  connectionButton.removeEventListener('click', connect);
+  connectionButton.innerHTML = 'DISCONNECT';
+  init();
 }
 
 
