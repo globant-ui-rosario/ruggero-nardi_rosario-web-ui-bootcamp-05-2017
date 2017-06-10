c87ec3e4ba3e736bbfa1f96a852cfd27eff1bd9b
Ruggero Nardi <s.ruggeronardi@gmail.com>
2017-06-10 17:39:34 -0300

    Moved square properties to an object

--- 04-HTML5-APIs/canvas.js
+++ 04-HTML5-APIs/canvas.js
@@ -1,4 +1,11 @@
 const drawArea = document.getElementById('drawing-area');
+const frame = window.requestAnimationFrame;
+let rectangle = {
+  x: 50,
+  y: 50,
+  width: 200,
+  height: 200,
+};
 function draw() {
   drawArea.width = 600;
   drawArea.height = 600;
@@ -15,7 +22,7 @@ function drawDivisionCanvas() {
   divisor.lineTo(300, 600);
   divisor.moveTo(0, 300);
   divisor.lineTo(600, 300);
-  divisor.lineWidth=2;
+  divisor.lineWidth = 2;
   divisor.strokeStyle = '#000000';
   divisor.stroke();
 }
@@ -24,16 +31,16 @@ function drawSquare() {
   square.lineWidth = 5;
   square.strokeStyle = randomColor();
   square.fillStyle = randomColor();
-  square.rect(50, 50, 200, 200);
+  square.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
   square.fill();
-  square.stroke();
-}
+  square.stroke()
+};
 function drawTriangle() {
   const triangle = drawArea.getContext('2d');
   triangle.beginPath();
-  triangle.moveTo(450,50);
-  triangle.lineTo(350,250);
-  triangle.lineTo(550,250);
+  triangle.moveTo(450, 50);
+  triangle.lineTo(350, 250);
+  triangle.lineTo(550, 250);
   triangle.closePath();
   triangle.strokeStyle = randomColor();
   triangle.fillStyle = randomColor();
@@ -43,7 +50,7 @@ function drawTriangle() {
 function drawCircle() {
   const circle = drawArea.getContext('2d');
   circle.beginPath();
-  circle.arc(150,450,100,0,2*Math.PI);
+  circle.arc(150, 450, 100, 0, 2 * Math.PI);
   circle.strokeStyle = randomColor();
   circle.fillStyle = randomColor();
   circle.fill();
@@ -52,16 +59,16 @@ function drawCircle() {
 function drawStar() {
   const star = drawArea.getContext('2d');
   star.beginPath();
-  star.moveTo(350,550);
-  star.lineTo(450,350);
-  star.lineTo(550,550);
-  star.lineTo(350,425);
-  star.lineTo(550,425);
+  star.moveTo(350, 550);
+  star.lineTo(450, 350);
+  star.lineTo(550, 550);
+  star.lineTo(350, 425);
+  star.lineTo(550, 425);
   star.closePath();
   star.strokeStyle = randomColor();
   star.fillStyle = randomColor();
   star.fill();
-  star.stroke(); 
+  star.stroke();
 }
 function randomColor() {
   let hexaValues = '789ABC';
